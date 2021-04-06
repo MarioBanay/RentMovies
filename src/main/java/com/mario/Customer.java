37295af01165770084087d439cd17c8a2c7b9815
mario-imac <banay.mario@gmail.com>
2021-04-06 14:29:21 +0200

    Customer class added

---
+++ src/main/java/com/mario/Customer.java
@@ -0,0 +1,20 @@
+package com.mario;
+
+import java.util.Vector;
+
+public class Customer {
+    private String name;
+    private Vector rentals = new Vector();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void addRental(Rental arg) {
+        rentals.addElement(arg);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
